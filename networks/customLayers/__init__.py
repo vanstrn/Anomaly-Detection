ee0686593b94297e6e92ca11a933b8cb09c709d7
Neale Van Stralen <>
2021-11-21 17:04:07 -0600

    Added init file to load custom layers

---
+++ networks/customLayers/__init__.py
@@ -0,0 +1,8 @@
+
+
+from .non_local import Non_local_nn
+from .approx_round import *
+from .inception import Inception
+from .reverse_inception import ReverseInception
+from .lstm_reshape import LSTM_Reshape,LSTM_Unshape
+from .split import Split,BasicCNNSplit,ChannelFilter
