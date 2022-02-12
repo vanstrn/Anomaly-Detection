2a91fa910a2586077c8c1959aa2e6eddf5cc6237
Neale Van Stralen <nealeav2@illinois.edu>
2022-02-12 17:27:16 -0600

    Added defualt AnoGAN implementation for learning latent space.

--- methods/AnoGAN.py
+++ methods/AnoGAN.py
@@ -20,6 +20,48 @@ class AnoGAN(GAN):
 
         self.HPs.update({
             "PredBatchSize":16,
+            "AnomalyFitEpochs":50,
+            "OptimizerLatent":"Adam",
+            "LearningRateLatent":1E-3
+            })
+        super().__init__(settingsDict,dataset,networkConfig)
+
+    def GenerateLatent(self,sample):
+        latentOptimizer = GetOptimizer(self.HPs["OptimizerLatent"],self.HPs["LearningRateLatent"])
+        z = tf.Variable(initial_value=tf.random.normal([sample.shape[0], self.HPs["LatentSize"]]),trainable=True)
+        for _ in range(self.HPs["AnomalyFitEpochs"]):
+            with tf.GradientTape() as tape:
+                out = self.Generator(z)["Decoder"]
+                latentLoss = tf.math.abs(out-sample)
+            latentGradients = tape.gradient(latentLoss, [z])
+            latentOptimizer.apply_gradients(zip(latentGradients, [z]))
+        return z
+
+    def ImagesFromImage(self,sample):
+        imagePredList = []
+        #Spliting data into appropriate batches and running them through the things
+        testDataset = tf.data.Dataset.from_tensor_slices(sample).batch(self.HPs["PredBatchSize"])
+        for batch in testDataset:
+            z = self.GenerateLatent(batch)
+            out=self.Generator(z)["Decoder"]
+            imagePredList.append(out)
+        return tf.concat(imagePredList,axis=0).numpy()
+
+    def DiscrimAnomaly(self,testImages):
+        return self.Discriminator.predict({"image":testImages})["Discrim"]
+
+    def ImageAnomaly(self,testImages):
+        return tf.reduce_sum((testImages-self.ImagesFromImage(testImages))**2,axis=list(range(1,len(testImages.shape))))
+
+    def AnomalyScore(self,testImages,alpha=0.9):
+        return tf.reduce_sum((testImages-self.ImagesFromImage(testImages))**2,axis=list(range(1,len(testImages.shape))))
+
+class AnoGAN_v2(GAN):
+    def __init__(self,settingsDict,dataset,networkConfig={}):
+        """Initializing Model and all Hyperparameters """
+
+        self.HPs.update({
+            "PredBatchSize":16,
             "AnomalyFitEpochs":10,
             })
         self.requiredParams.Append(["LatentNetworkConfig"])
@@ -52,5 +94,12 @@ class AnoGAN(GAN):
             i+=batch.shape[0]
         return imagePred
 
-    def AnomalyScore(self,testImages):
+    def DiscrimAnomaly(self,testImages):
+        z = self.Encoder.predict({"image":testImages})["Latent"]
+        return self.Discriminator.predict({"image":testImages,"features":z})["Discrim"]
+
+    def ImageAnomaly(self,testImages):
+        return tf.reduce_sum((testImages-self.ImagesFromImage(testImages))**2,axis=list(range(1,len(testImages.shape))))
+
+    def AnomalyScore(self,testImages,alpha=0.9):
         return tf.reduce_sum((testImages-self.ImagesFromImage(testImages))**2,axis=list(range(1,len(testImages.shape))))
