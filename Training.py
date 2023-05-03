8188790755d1c17241d20f8f5bf2f6f83fdbd502
Neale Van Stralen <nealeav2@illinois.edu>
2023-05-02 20:50:15 -0500

    Created better structure for the logging.

--- Training.py
+++ Training.py
@@ -1,36 +1,60 @@
-
-import argparse
-import tensorflow as tf
+#!/usr/bin/env python
+# Created By  : Neale Van Stralen
+# version ='1.0'
+# ---------------------------------------------------------------------------
+"""
+Main executable for training of machine vision experiments.
+"""
 import os
-import json
-import numpy as np
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import logging
-from utils.utils import LoadConfig,CreatePath,GetFunction,UpdateNestedDictionary
-from utils.utils import LoadDataset,Logger,JSON_Load
-from methods.utils import LoadMethod
-
-log = logging.getLogger(__name__)
-
+import argparse
+from utils.utils import JSON_Load,LoadConfig,GetFunction,UpdateNestedDictionary
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--configFile", required=True,
-                    help="Filename or filepath to be run.")
+    help="Filename or filepath to be run.")
 parser.add_argument("-c", "--config", required=False, default={}, action=JSON_Load,
-                    help="Specify overrides to be made to the run.")
+    help="Specify overrides to be made to the run.")
 parser.add_argument("-n", "--network", required=False, default={}, action=JSON_Load,
-                    help="Specify special parameters for the network.")
+    help="Specify special parameters for the network.")
 parser.add_argument("-p", "--processor", required=False, default="gpu0",
-                    help="Processor identifier string. [cpu / gpu0 / gpu1]")
+    help="Processor identifier string. [cpu / gpu0 / gpu1]")
 parser.add_argument("--test", required=False,
-                    action="store_true",help="Flag to skip testing")
+    action="store_true",help="Flag to skip testing")
 parser.add_argument("--seed", required=False,
-                    default=None, help="Seed input to set tensorflow and numpy seed.")
+    default=None, help="Seed input to set tensorflow and numpy seed.")
 args = parser.parse_args()
-
 #Reading in the config files.
 settings = LoadConfig(args.configFile)
 settings = UpdateNestedDictionary(settings,args.config)
 
-#Setting up tensorflow based on specified processor configur
+LOG_PATH = './logs/'+settings["RunName"]
+
+logging.basicConfig(
+    level=logging.INFO,
+    # format='%(levelname)s -- %(asctime)s -- %(message)s',
+    format='%(levelname)s - %(asctime)s - (%(filename)s:%(lineno)d) - %(message)s ',
+    # format=CustomFormatter(),
+    handlers=[
+        logging.StreamHandler(),
+        logging.FileHandler(LOG_PATH + '/logger.log'),
+        ],
+    datefmt='%Y/%m/%d %I:%M:%S %p'
+    )
+logger = logging.getLogger("Trainer")
+
+logger.info("Running experiment with the following config properties:\n{}".format(pformat(settings)))
+
+import tensorflow as tf
+import numpy as np
+import random
+from pprint import pformat
+
+from utils.logging import ExperimentLogger
+from methods.utils import LoadMethod
+from datasets import LoadDataset
+
+#Setting up tensorflow based on specified processor configuration
 if "gpu" in args.processor:
     gpus = tf.config.experimental.list_physical_devices('GPU')
     for gpu in gpus:
@@ -38,16 +62,20 @@ if "gpu" in args.processor:
 else:
     os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 tf.keras.backend.set_floatx('float32')
+logger.info("Running the expeiment with {}. CUDA_VISIBLE_DEVICES={} \n\t Actual GPUs: {}".format(args.processor,os.getenv("CUDA_VISIBLE_DEVICES"),gpus))
 
-#Creating specific save folders for the different
-logger = Logger('./logs/'+settings["RunName"])
-logger.RecordGitState()
+#Creating experiment logger which with functions for saving tensorboard logs and images
+expLogger = ExperimentLogger(LOG_PATH)
+expLogger.RecordGitState()
 
-log.info("Running the expeiment with {}. CUDA_VISIBLE_DEVICES={}".format(args.processor,os.getenv("CUDA_VISIBLE_DEVICES")))
 
-if args.seed is None:
-    tf.random.set_seed(args.seed)
-    np.random.seed(args.seed)
+if args.seed is not None:
+    seed = args.seed
+else:
+    seed = random.randint(0, 1000000)
+logger.info("Running experiment using seed: `{}`".format(seed))
+tf.random.set_seed(args.seed)
+np.random.seed(args.seed)
 
 try:
     dataset = LoadDataset(settings)
@@ -58,7 +86,7 @@ try:
     if "Callbacks" in settings:
         for dict in settings["Callbacks"]:
             func = GetFunction(dict["Name"])
-            callbacks.append(func(logger,dataset,**dict["Arguments"]))
+            callbacks.append(func(expLogger,dataset,**dict["Arguments"]))
 
     if args.test:
         #Skipping Training and going straight to testing. This calls "On Train End" Callbacks
@@ -68,7 +96,7 @@ try:
 
 
 except Exception as e:
-    log.warning("Closing environment due to error")
-    log.error('Error', exc_info=e)
+    logger.warning("Closing environment due to error")
+    logger.error('Error', exc_info=e)
 except KeyboardInterrupt:
-    log.warning("Closing environment due to Keyboard Interrupt")
+    logger.warning("Closing environment due to User Interrupt")
