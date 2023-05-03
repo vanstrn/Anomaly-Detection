ac51abfeea7894a0aa741d8e8beff8999acd5e3e
Neale Van Stralen <nealeav2@illinois.edu>
2023-05-02 20:50:37 -0500

    Moved Dataset loader to dataset folder.

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,3 +1,7 @@
 from .mnist import *
 from .fmnist import *
 from .cifar import *
+
+def LoadDataset(settings,**kwargs):
+    func = eval(settings["Dataset"]["Name"])
+    return func(**settings["Dataset"]["Arguments"])
