4c495d0e132319e7c8b146af8e01a892b42424ae
Neale Van Stralen <nealeav2@illinois.edu>
2023-05-26 16:50:42 -0500

    Updated hyperparameter tags.

--- scripts/AnomalyDetection/ganomaly_experiment.sh
+++ scripts/AnomalyDetection/ganomaly_experiment.sh
@@ -43,13 +43,13 @@ case "$seed" in
 
 esac
 
-$CUDA_STRING python Training.py -f Ganomaly.json -c '{"RunName":"Ganomaly_holdout_0'$batchID'","NetworkHPs":{'$hyperparams'},"Dataset":{"Arguments":{"holdout":0}}}' $SEED_STRING $args $CPU_STRING
-$CUDA_STRING python Training.py -f Ganomaly.json -c '{"RunName":"Ganomaly_holdout_1'$batchID'","NetworkHPs":{'$hyperparams'},"Dataset":{"Arguments":{"holdout":1}}}' $SEED_STRING $args $CPU_STRING
-$CUDA_STRING python Training.py -f Ganomaly.json -c '{"RunName":"Ganomaly_holdout_2'$batchID'","NetworkHPs":{'$hyperparams'},"Dataset":{"Arguments":{"holdout":2}}}' $SEED_STRING $args $CPU_STRING
-$CUDA_STRING python Training.py -f Ganomaly.json -c '{"RunName":"Ganomaly_holdout_3'$batchID'","NetworkHPs":{'$hyperparams'},"Dataset":{"Arguments":{"holdout":3}}}' $SEED_STRING $args $CPU_STRING
-$CUDA_STRING python Training.py -f Ganomaly.json -c '{"RunName":"Ganomaly_holdout_4'$batchID'","NetworkHPs":{'$hyperparams'},"Dataset":{"Arguments":{"holdout":4}}}' $SEED_STRING $args $CPU_STRING
-$CUDA_STRING python Training.py -f Ganomaly.json -c '{"RunName":"Ganomaly_holdout_5'$batchID'","NetworkHPs":{'$hyperparams'},"Dataset":{"Arguments":{"holdout":5}}}' $SEED_STRING $args $CPU_STRING
-$CUDA_STRING python Training.py -f Ganomaly.json -c '{"RunName":"Ganomaly_holdout_6'$batchID'","NetworkHPs":{'$hyperparams'},"Dataset":{"Arguments":{"holdout":6}}}' $SEED_STRING $args $CPU_STRING
-$CUDA_STRING python Training.py -f Ganomaly.json -c '{"RunName":"Ganomaly_holdout_7'$batchID'","NetworkHPs":{'$hyperparams'},"Dataset":{"Arguments":{"holdout":7}}}' $SEED_STRING $args $CPU_STRING
-$CUDA_STRING python Training.py -f Ganomaly.json -c '{"RunName":"Ganomaly_holdout_8'$batchID'","NetworkHPs":{'$hyperparams'},"Dataset":{"Arguments":{"holdout":8}}}' $SEED_STRING $args $CPU_STRING
-$CUDA_STRING python Training.py -f Ganomaly.json -c '{"RunName":"Ganomaly_holdout_9'$batchID'","NetworkHPs":{'$hyperparams'},"Dataset":{"Arguments":{"holdout":9}}}' $SEED_STRING $args $CPU_STRING
+$CUDA_STRING python Training.py -f Ganomaly.json -c '{"RunName":"Ganomaly_holdout_0'$batchID'","HyperParams":{'$hyperparams'},"Dataset":{"Arguments":{"holdout":0}}}' $SEED_STRING $args $CPU_STRING
+$CUDA_STRING python Training.py -f Ganomaly.json -c '{"RunName":"Ganomaly_holdout_1'$batchID'","HyperParams":{'$hyperparams'},"Dataset":{"Arguments":{"holdout":1}}}' $SEED_STRING $args $CPU_STRING
+$CUDA_STRING python Training.py -f Ganomaly.json -c '{"RunName":"Ganomaly_holdout_2'$batchID'","HyperParams":{'$hyperparams'},"Dataset":{"Arguments":{"holdout":2}}}' $SEED_STRING $args $CPU_STRING
+$CUDA_STRING python Training.py -f Ganomaly.json -c '{"RunName":"Ganomaly_holdout_3'$batchID'","HyperParams":{'$hyperparams'},"Dataset":{"Arguments":{"holdout":3}}}' $SEED_STRING $args $CPU_STRING
+$CUDA_STRING python Training.py -f Ganomaly.json -c '{"RunName":"Ganomaly_holdout_4'$batchID'","HyperParams":{'$hyperparams'},"Dataset":{"Arguments":{"holdout":4}}}' $SEED_STRING $args $CPU_STRING
+$CUDA_STRING python Training.py -f Ganomaly.json -c '{"RunName":"Ganomaly_holdout_5'$batchID'","HyperParams":{'$hyperparams'},"Dataset":{"Arguments":{"holdout":5}}}' $SEED_STRING $args $CPU_STRING
+$CUDA_STRING python Training.py -f Ganomaly.json -c '{"RunName":"Ganomaly_holdout_6'$batchID'","HyperParams":{'$hyperparams'},"Dataset":{"Arguments":{"holdout":6}}}' $SEED_STRING $args $CPU_STRING
+$CUDA_STRING python Training.py -f Ganomaly.json -c '{"RunName":"Ganomaly_holdout_7'$batchID'","HyperParams":{'$hyperparams'},"Dataset":{"Arguments":{"holdout":7}}}' $SEED_STRING $args $CPU_STRING
+$CUDA_STRING python Training.py -f Ganomaly.json -c '{"RunName":"Ganomaly_holdout_8'$batchID'","HyperParams":{'$hyperparams'},"Dataset":{"Arguments":{"holdout":8}}}' $SEED_STRING $args $CPU_STRING
+$CUDA_STRING python Training.py -f Ganomaly.json -c '{"RunName":"Ganomaly_holdout_9'$batchID'","HyperParams":{'$hyperparams'},"Dataset":{"Arguments":{"holdout":9}}}' $SEED_STRING $args $CPU_STRING
